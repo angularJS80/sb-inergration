ee8c338dd2716ffa3aed261a86d438792f37d6c3
choyongbeom <tutuatutu1@gmail.com>
2018-06-14 09:05:50 +0900

    Project Start

---
+++ src/main/java/com/sts/pjtry/SbtryApplication.java
@@ -0,0 +1,12 @@
+package com.sts.pjtry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbtryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbtryApplication.class, args);
+	}
+}
