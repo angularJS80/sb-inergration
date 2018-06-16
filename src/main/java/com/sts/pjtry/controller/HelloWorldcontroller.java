fac051c1e74b8afbff07b05922e0d9190c05ee96
angularJS80 <bigcen@naver.com>
2018-06-16 20:49:42 +0900

    금일 학습내용 주석 및 정

--- src/main/java/com/sts/pjtry/controller/HelloWorldcontroller.java
+++ src/main/java/com/sts/pjtry/controller/HelloWorldcontroller.java
@@ -20,28 +20,42 @@ public class HelloWorldcontroller {
 	
 	public static Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss.SSS").create(); 
 	
+	
 	@RequestMapping("/hellow")
 	public String Hello(Map<String,Object> model) {
 		model.put("message", "Hellow World");
 		return "welcome";
 	}
 	
+	//국가를 구분할 String 인자값을 클라이언트로 받아서 국가별 인사를 리턴하는 컨트롤
 	@RequestMapping(value="/hellowGetJson", method=RequestMethod.GET)
-	    public @ResponseBody  String Hello(@RequestParam String contry, HttpServletRequest request) {
+	public @ResponseBody  String Hello(@RequestParam String contry, HttpServletRequest request) {
 	    	Map<String, Object> rtnMap = new HashMap<String, Object>();
-	    	rtnMap.put("korea", "안녕하세요");
+	    String rtnStr =getHellowMap().get(contry);
+	    rtnMap.put("result", rtnStr);
+	    rtnMap.put("contry", contry);
+	    rtnMap.put("comment", "현재의 결과값을 복사하여 hellowPostJson 에 입력값으로 사용하세요");
 			return gson.toJson(rtnMap);
 	}
-	 
-	
-    
-    @RequestMapping(value="/hellowPostJson", method=RequestMethod.POST)
+
+    //위와 동일하나 포스트 방식으로 더많은 인자값을 받을 수 있다.(Json 형태로 MessageConverter 가 알아서 인자값의 형태(paramMap) 으로변환해준다. 
+	@RequestMapping(value="/hellowPostJson", method=RequestMethod.POST)
     public @ResponseBody  String Hello(@RequestBody Map<String, Object> paramMap, HttpServletRequest request) {
-    	Map<String, Object> rtnMap = new HashMap<String, Object>();
-    	rtnMap.put("korea", "안녕하세요");
+    		
+		String contry = (String)paramMap.get("contry");
+		Map<String, Object> rtnMap = new HashMap<String, Object>();
+	    String rtnStr =getHellowMap().get(contry);
+	    rtnMap.put("result", rtnStr);
 		return gson.toJson(rtnMap);
 	}
-    
+	
+	//국가별 인사말배열을 제공하는 메써
+    private Map<String, String> getHellowMap() {
+	    	HashMap<String,String> hellowMap = new HashMap<String,String>();
+	    	hellowMap.put("usa", "hellow");
+	    	hellowMap.put("korea", "안녕하세");
+	    	return hellowMap;
+	}
 	
 	@RequestMapping("/")
 	 public String index(){
